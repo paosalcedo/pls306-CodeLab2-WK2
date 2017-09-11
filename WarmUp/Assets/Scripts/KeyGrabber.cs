26ab70e8aeeee39a418737a5036abc26a0f105ea
paosalcedo @ NYU <pls306@nyu.edu>
2017-09-11 17:13:05 -0400

    Added project from class

---
+++ WarmUp/Assets/Scripts/KeyGrabber.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyGrabber : MonoBehaviour {
+
+	public List<Vector3> hFormation = new List<Vector3>();
+	public List<Vector3> eFormation = new List<Vector3>();
+	public List<Vector3> lFormation = new List<Vector3>();
+	public List<Vector3> oFormation = new List <Vector3>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
